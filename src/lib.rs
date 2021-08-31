c94a3a690a1897a63264bb31c87cae4302bcbe65
nicolas cros <ncros@mbp-de-ncros.home>
2021-08-31 08:02:19 +0200

    add flag register

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::convert;
+
 const ZERO_BIT: u8 = 7;
 const SUBSTRACTION_BIT: u8 = 6;
 const HALF_CARRY_BIT: u8 = 5;
@@ -11,31 +13,38 @@ pub struct Registers {
     e: u8,
     f: u8,
     h: u8,
-    l: u8
+    l: u8,
 }
 
 struct FlagRegister {
     zero: bool,
     substraction: bool,
     half_carry: bool,
-    carry: bool
+    carry: bool,
 }
 
 impl std::convert::From<FlagRegister> for u8 {
     fn from(flag: FlagRegister) -> u8 {
-        (if flag.zero {1} else {0}) << ZERO_BIT |
-        (if flag.substraction {1} else {0}) << SUBSTRACTION_BIT |
-        (if flag.half_carry {1} else {0}) << HALF_CARRY_BIT |
-        (if flag.carry {1} else {0}) << CARRY_BIT
+        (if flag.zero { 1 } else { 0 }) << ZERO_BIT
+            | (if flag.substraction { 1 } else { 0 }) << SUBSTRACTION_BIT
+            | (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_BIT
+            | (if flag.carry { 1 } else { 0 }) << CARRY_BIT
     }
 }
 
 impl std::convert::From<u8> for FlagRegister {
     fn from(byte: u8) -> FlagRegister {
-        ((byte && 0x80) >> ZERO_BIT) |
-        ((byte && 0x40) >> SUBSTRACTION_BIT) |
-        ((byte && 0x20) >> HALF_CARRY_BIT) |
-        ((byte && 0x10) >> CARRY_BIT)
+        let zero = ((byte & 0x80) >> ZERO_BIT) != 0;
+        let substraction = ((byte & 0x40) >> SUBSTRACTION_BIT) != 0;
+        let half_carry = ((byte & 0x20) >> HALF_CARRY_BIT) != 0;
+        let carry = ((byte & 0x10) >> CARRY_BIT) != 0;
+
+        FlagRegister {
+            zero: zero,
+            substraction: substraction,
+            half_carry: half_carry,
+            carry: carry,
+        }
     }
 }
 
@@ -74,15 +83,15 @@ mod tests {
 
     #[test]
     fn test_bc() {
-        let mut regs = Registers { 
-            a: 10, 
-            b: 1, 
-            c: 3, 
-            d: 4, 
-            e: 5, 
-            f: 6, 
-            h: 7, 
-            l: 8
+        let mut regs = Registers {
+            a: 10,
+            b: 1,
+            c: 3,
+            d: 4,
+            e: 5,
+            f: 6,
+            h: 7,
+            l: 8,
         };
         regs.set_bc(0b0011_1000);
         assert_eq!(regs.get_bc(), 0b0011_1000);
@@ -90,15 +99,15 @@ mod tests {
 
     #[test]
     fn test_af() {
-        let mut regs = Registers { 
-            a: 10, 
-            b: 1, 
-            c: 3, 
-            d: 4, 
-            e: 5, 
-            f: 6, 
-            h: 7, 
-            l: 8
+        let mut regs = Registers {
+            a: 10,
+            b: 1,
+            c: 3,
+            d: 4,
+            e: 5,
+            f: 6,
+            h: 7,
+            l: 8,
         };
         regs.set_af(0b0010_1101);
         assert_eq!(regs.get_af(), 0b0010_1101);
@@ -106,17 +115,39 @@ mod tests {
 
     #[test]
     fn test_de() {
-        let mut regs = Registers { 
-            a: 10, 
-            b: 1, 
-            c: 3, 
-            d: 4, 
-            e: 5, 
-            f: 6, 
-            h: 7, 
-            l: 8
+        let mut regs = Registers {
+            a: 10,
+            b: 1,
+            c: 3,
+            d: 4,
+            e: 5,
+            f: 6,
+            h: 7,
+            l: 8,
         };
         regs.set_de(0b1010_1001);
         assert_eq!(regs.get_de(), 0b1010_1001);
+    }
+
+    #[test]
+    fn test_flag() {
+        let flag_byte = u8::from(FlagRegister {
+            zero: true,
+            substraction: false,
+            half_carry: true,
+            carry: true,
+        });
+
+        assert_eq!(flag_byte, 0b1011_0000);
+    }
+
+    #[test]
+    fn test_flag_ex() {
+        let flag_reg = FlagRegister::from(0b0101_0000);
+
+        assert_eq!(flag_reg.zero, false);
+        assert_eq!(flag_reg.substraction, true);
+        assert_eq!(flag_reg.half_carry, false);
+        assert_eq!(flag_reg.carry, true);
     }
 }
