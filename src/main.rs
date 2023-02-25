9681a4e935c176d5462adf2fcd6416f04048bc12
nchf3 <nicolas.cros3@orange.fr>
2023-02-25 17:12:55 +0100

    use a function to parse command line args

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,31 @@ const WINDOW_DIMENSIONS: [usize; 2] = [(SCREEN_WIDTH * SCALE_FACTOR), (SCREEN_HE
 
 fn main() {
     // get arguments from the command line   
+    let (boot_rom_path, game_rom_path, debug_mode) = parse_args();
+
+    let mut file = File::open(boot_rom_path).unwrap();
+    let mut bin_data = [0xFF as u8; 256];
+    if let Err(message) = file.read_exact(&mut bin_data) {
+        panic!("Cannot read file with error message: {}", message);
+    }
+
+    let mut rom_file = File::open(game_rom_path).unwrap();
+    let mut rom_data = [0xFF as u8; 32768];
+    if let Err(message) = rom_file.read_exact(&mut rom_data) {
+        panic!("Cannot read file with error message: {}", message);
+    }
+    println!("rom file len: {:#06x}", rom_file.metadata().unwrap().len());
+
+    // create the emulated system
+    let mut emulator = Emulator::new(&bin_data, &rom_data);
+
+    // run the emulator
+    manage_window(&mut emulator);
+}
+
+fn parse_args() -> (String, String, bool) {
     let mut boot_rom_path = String::new();
-    let mut rom_path = String::new();
+    let mut game_rom_path = String::new();
     let mut debug_opt = false;
 
     for (index, argument) in env::args().enumerate() {
@@ -23,8 +46,8 @@ fn main() {
                 println!("boot_rom: {}", boot_rom_path);
             }
             2 => {
-                rom_path = argument.clone();
-                println!("game_rom: {}", rom_path);
+                game_rom_path = argument.clone();
+                println!("game_rom: {}", game_rom_path);
             }
             3 => if argument.eq("--debug") {
                     debug_opt = true;
@@ -39,25 +62,7 @@ fn main() {
         println!("emulator mode: normal");
     }
 
-    // let mut file = File::open("../gb-test-roms/cpu_instrs/cpu_instrs.gb").unwrap();
-    let mut file = File::open(boot_rom_path).unwrap();
-    let mut bin_data = [0xFF as u8; 256];
-    if let Err(message) = file.read_exact(&mut bin_data) {
-        panic!("Cannot read file with error message: {}", message);
-    }
-
-    let mut rom_file = File::open(rom_path).unwrap();
-    let mut rom_data = [0xFF as u8; 32768];
-    if let Err(message) = rom_file.read_exact(&mut rom_data) {
-        panic!("Cannot read file with error message: {}", message);
-    }
-    println!("rom file len: {:#06x}", rom_file.metadata().unwrap().len());
-
-    // create the emulated system
-    let mut emulator = Emulator::new(&bin_data, &rom_data);
-
-    // run the emulator
-    manage_window(&mut emulator);
+    (boot_rom_path, game_rom_path, debug_opt)
 }
 
 fn manage_window(emulator: &mut Emulator) {
