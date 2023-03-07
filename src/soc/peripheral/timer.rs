bd93aa3fe0857fc9255e1b3c08ce21f4a1165834
nchf3 <nicolas.cros3@orange.fr>
2023-03-07 10:52:15 +0100

    delay timer interrupt from 1 machine cycle)

--- src/soc/peripheral/timer.rs
+++ src/soc/peripheral/timer.rs
@@ -1,4 +1,5 @@
 use crate::soc::peripheral::nvic::{Nvic, InterruptSources};
+use crate::soc::CLOCK_TICK_PER_MACHINE_CYCLE;
 
 pub enum Frequency {
     F4096,
@@ -25,11 +26,12 @@ pub struct Timer {
     // internal parameters
     main_timer_cycles: usize,
     divider_timer_cycles: usize,
-    // timer values
+    tima_overflow: bool,
+    // DIV / TIMA / TMA registers
     pub divider: u8,
     pub value: u8,
     pub modulo: u8,
-    // control register
+    // TAC registers values
     pub main_timer_frequency: Frequency,
     pub divider_timer_frequency: Frequency,
     pub enabled: bool,
@@ -38,11 +40,15 @@ pub struct Timer {
 impl Timer {
     pub fn new() -> Timer {
         Timer {
+            // internal parameters
             main_timer_cycles: 0,
             divider_timer_cycles: 0,
+            tima_overflow: false,
+            // DIV / TIMA / TMA registers
             divider: 0,
             value: 0,
             modulo: 0,
+            // TAC registers values
             main_timer_frequency: Frequency::F4096,
             divider_timer_frequency: Frequency::F16384,
             enabled: false,
@@ -55,6 +61,13 @@ impl Timer {
             self.main_timer_cycles += cycles as usize;
             self.divider_timer_cycles += cycles as usize;
 
+            // delay interrupt by 1 machine cycle / 4 clocks
+            if self.tima_overflow && self.has_timer_passed_1_cycle() {
+                self.tima_overflow = false;
+                nvic.set_interrupt(InterruptSources::TIMER);
+                self.value = self.modulo;
+            }
+
             // divide the main cpu clock
             let main_cycles_per_tick = self.main_timer_frequency.cycles_per_tick();
             if self.main_timer_cycles > main_cycles_per_tick {
@@ -64,7 +77,16 @@ impl Timer {
                 let (new_value, overflow) = self.value.overflowing_add(1);
                 self.value = new_value;
 
+                // register overflow for next cycle if any
+                // see https://gbdev.io/pandocs/Timer_Obscure_Behaviour.html 
                 if overflow {
+                    self.tima_overflow = true;
+                    self.value = 0;
+                }
+
+                // delay interrupt by 1 machine cycle / 4 clocks
+                if self.tima_overflow && self.has_timer_passed_1_cycle() {
+                    self.tima_overflow = false;
                     nvic.set_interrupt(InterruptSources::TIMER);
                     self.value = self.modulo;
                 }
@@ -80,6 +102,10 @@ impl Timer {
                 self.divider = new_divider;
             } 
         }
+    }
+
+    pub fn has_timer_passed_1_cycle(&self) -> bool {
+        self.main_timer_cycles / CLOCK_TICK_PER_MACHINE_CYCLE as usize > 0
     }
 
     pub fn set_divider(&mut self) {
@@ -153,6 +179,10 @@ mod timer_tests {
         }
 
         assert_eq!(timer.value, 0x00);
+        assert_eq!(nvic.get_interrupt(), None);
+
+        // run 1 more machine cycle (4 clocks) to rise the interrupt
+        timer.run(CLOCK_TICK_PER_MACHINE_CYCLE, &mut nvic);
         assert_eq!(nvic.get_interrupt().unwrap(), InterruptSources::TIMER);
 
         timer.modulo = 0xF5;
